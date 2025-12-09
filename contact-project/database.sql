550bc75155d50273ce3d92623930f61a7b6f58db
zeeshan-2004 <fareedzeeshan868@gmail.com>
2025-12-09 20:46:33 +0500

    Delete contact-project directory

--- contact-project/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS contact_manager;
-USE contact_manager;
-
-CREATE TABLE IF NOT EXISTS contacts (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    phone VARCHAR(20) NOT NULL
-);
